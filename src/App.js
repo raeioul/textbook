41f542737bd6165548651d4481211c91f060b95b
Raúl Acuña <raeioul@gmail.com>
2020-02-14 16:00:07 -0400

    Configure redux, connect store and add styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CourseList from "./components/CourseList.jsx";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addCourse, deleteCourse, updateCourse } from "./actions/courseActions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addNewCourse = this.addNewCourse.bind(this);
+    this.deleteCourse = this.deleteCourse.bind(this);
+    this.editCourseSubmit = this.editCourseSubmit.bind(this);
+  }
+  componentWillMount() {}
+  addNewCourse() {
+    this.props.addCourse({
+      id:
+        Math.max(
+          ...this.props.courseList.map(function(o) {
+            return o.id;
+          })
+        ) + 1,
+      name: "",
+      description: 1,
+      textbooks: ""
+    });
+  }
+
+  deleteCourse(id) {
+    let r = window.confirm("Do you want to delete this item");
+    if (r === true) {
+      this.props.deleteCourse(id);
+    }
+  }
+  editCourseSubmit(id, name, description, textbooks) {
+    this.props.updateCourse({
+      id: id,
+      name: name,
+      description: description,
+      textbooks: textbooks
+    });
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row mt-3">
+          <div className="col-lg-12">
+            <div className="card">
+              <div className="card-header">Course Registry</div>
+              <div className="card-body">
+                <table className="table table-hover">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Textbooks (autor/title)</th>
+                      <th>Edit/Save</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <CourseList
+                    deleteCourse={this.deleteCourse}
+                    courseList={this.props.courseList}
+                    editCourseSubmit={this.editCourseSubmit}
+                  />
+                </table>
+                <button
+                  className="btn btn-dark pull-left"
+                  onClick={this.addNewCourse}
+                >
+                  Add New
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    courseList: state
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      addCourse: addCourse,
+      deleteCourse: deleteCourse,
+      updateCourse: updateCourse
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import courseReducer from "./reducers/courseReducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let initialState = [
+  {
+    id: 1,
+    name: "John Doe",
+    description: 1,
+    textbooks: [{ author: "Makelele Claude", title: "Real Madrid" }]
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    description: 2,
+    textbooks: [{ author: "Raúl Acuña", title: "Wilstermann" }]
+  },
+  {
+    id: 3,
+    name: "Terry Adams",
+    description: 3,
+    textbooks: [
+      { author: "Makelele Claude", title: "Real Madrid" },
+      { author: "Makelele Claude", title: "Real Madrid" }
+    ]
+  },
+  { id: 4, name: "Jenny Smith", description: 4 }
+];
+
+if (localStorage.getItem("courses") === null)
+  localStorage.setItem("courses", JSON.stringify(initialState));
+else initialState = JSON.parse(localStorage.getItem("courses"));
+
+const store = createStore(courseReducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
